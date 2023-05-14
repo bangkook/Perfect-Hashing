bdeafb8598168bbaa0b868ccc099c1f9d99bf410
emansamir2 <51717395+emansamir2@users.noreply.github.com>
2023-05-15 00:00:36 +0300

    Revert "Merge branch 'main' of https://github.com/bangkook/Perfect-Hashing"
    This reverts commit 2ac0b9c0d8540d789483e490a5130a0004ace8c2, reversing
    changes made to 13920972e641348b8774d7f9c53c65a782bc7fc7.

--- src/main/java/N_linear.java
+++ src/main/java/N_linear.java
@@ -1,51 +1,20 @@
 import java.util.ArrayList;
 
-public class N_linear implements PerfectHashing {
+public class N_linear implements PerfectHashing{
     int N;
-    String[] hash;
-    ArrayList<String>[] Level2Hash;
+    ArrayList<String>[] hash;
     MatHash matHash;
-    MatHash[] matHashes;
 
     public N_linear(int n) {
         this.N = n;
         int M = N; // M = N
-        matHash = new MatHash((int) (Math.log(M) / Math.log(2))); // hash level 1 function
-        hash = new String[N];
-        Level2Hash = new ArrayList[N];
-        for (int i = 0; i < N; i++) {
-            Level2Hash[i] = new ArrayList<String>();
-        }
-        matHashes = new MatHash[N];
+        matHash = new MatHash((int)(Math.log(M)/Math.log(2)));
+        hash = new ArrayList[N];
     }
 
     @Override
     public boolean insert(String key) {
-        int index = matHash.hash(key); // get key of the inserted element
-        System.out.println("index" + index);
-        if(search(key)){//TODO 
-            return false;
-        }
-        if (hash[index] == null) {
-            hash[index] = key;
-        } else {
-            System.out.println(Level2Hash[index].size());
-            int count = (int) Math.sqrt(Level2Hash[index].size());
-            count++;
-            count = count * count;
-            Level2Hash[index].ensureCapacity(count);
-            matHashes[index] = new MatHash((int) (Math.log(count) / Math.log(2)));
-            int index2 = matHashes[index].hash(key); // get key of the inserted element
-            for (int i = 0; i < count; i++) {
-                if(i == index2)
-                Level2Hash[index].add(index2, key); // Add element at the specified index
-                else
-                    Level2Hash[index].add(i,  null); // Add element at the specified index
-            }
-            System.out.println("size" + Level2Hash[index].size());
-
-        }
-        return true;
+        return false;
     }
 
     @Override
@@ -56,28 +25,5 @@ public class N_linear implements PerfectHashing {
     @Override
     public boolean search(String key) {
         return false;
-    }
-    private void printHash(){
-        for (int i = 0; i < N; i++) {
-            System.out.print(hash[i] +">>");
-            for (int j = 0; j < Level2Hash[i].size(); j++) {
-                System.out.print("["+ Level2Hash[i].get(j) +"]" + " ");
-            }
-            System.out.println("");
-        }
-    }
-    public static void main(String[] args) {
-        N_linear test = new N_linear(20);
-        test.insert("nancy");
-        test.insert("sara");
-        test.insert("saafasdfra");
-        test.insert("ssdfsdara");
-        test.insert("safdsfra");
-        test.insert("saefwqera");
-        test.insert("sfdsara");
-        test.insert("sardfsdfdsfsdfa");
-
-
-        test.printHash();
     }
 }
