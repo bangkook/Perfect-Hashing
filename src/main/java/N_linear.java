d404608ad2aef5494d2bea1372eb58a506b33829
emansamir2 <51717395+emansamir2@users.noreply.github.com>
2023-05-20 03:46:00 +0300

    jnjnjnkjnkllkjm

--- src/main/java/N_linear.java
+++ src/main/java/N_linear.java
@@ -38,6 +38,7 @@ public class N_linear implements PerfectHashing {
             count = count * count;
             boolean flag = true;
             while (flag) {
+                flag = false;
                 matHashes[index] = new MatHash((int) (Math.log(count) / Math.log(2)));
                 ArrayList<String> ReHach = new ArrayList<String>(count);
                 for (int i = 0; i < count; i++) {
@@ -46,14 +47,24 @@ public class N_linear implements PerfectHashing {
                 for (int i = 0; i < Level2Hash[index].size(); i++) {
                     String element = Level2Hash[index].get(i);
                     if (element != null) {
-                        ReHach.set(matHashes[index].hash(element), element);
+                        int temp=matHashes[index].hash(element);
+                        if(ReHach.get(temp)==null) ReHach.set(temp, element);
+                        else{
+                            flag=true;
+                            break;
+                        }
                     }
+                }
+                if(flag){
+                    continue;
                 }
                 Level2Hash[index] = ReHach;
                 int index2 = matHashes[index].hash(key); // get key of the inserted element
                 if (Level2Hash[index].get(index2) == null) {
                     Level2Hash[index].set(index2, key); // Sets element at the specified index
                     flag = false;
+                }else{
+                    flag=true;
                 }
             }
         }
@@ -61,25 +72,50 @@ public class N_linear implements PerfectHashing {
         return true;
     }
 
-    public ArrayList<String> rehashing(int size, int index){
-        matHashes[index] = new MatHash((int) (Math.log(size) / Math.log(2)));
-        ArrayList<String> ReHach = new ArrayList<String>(size);
-        for (int i = 0; i < size; i++) {
-            ReHach.add(i, null);
-        }
-        for (int i = 0; i < Level2Hash[index].size(); i++) {//rehaching the past elements
-            String element = Level2Hash[index].get(i);
-            if(element != null){
-                ReHach.set(matHashes[index].hash(element), element); // Sets element at the specified index
-                //System.out.println("matHashes = "+matHashes[index].hash(element));
+    public ArrayList<String> rehashing(int count, int index){
+//        matHashes[index] = new MatHash((int) (Math.log(size) / Math.log(2)));
+//        ArrayList<String> ReHach = new ArrayList<String>(size);
+//        for (int i = 0; i < size; i++) {
+//            ReHach.add(i, null);
+//        }
+//        for (int i = 0; i < Level2Hash[index].size(); i++) {//rehaching the past elements
+//            String element = Level2Hash[index].get(i);
+//            if(element != null){
+//                ReHach.set(matHashes[index].hash(element), element); // Sets element at the specified index
+//                //System.out.println("matHashes = "+matHashes[index].hash(element));
+//            }
+//        }
+        ArrayList<String> ReHach=new ArrayList<String>(count);
+        boolean flag = true;
+        while (flag) {
+            flag = false;
+            matHashes[index] = new MatHash((int) (Math.log(count) / Math.log(2)));
+            ReHach.clear();
+            for (int i = 0; i < count; i++) {
+                ReHach.add(i, null);
             }
+            for (int i = 0; i < Level2Hash[index].size(); i++) {
+                String element = Level2Hash[index].get(i);
+                if (element != null) {
+                    int temp=matHashes[index].hash(element);
+                    if(ReHach.get(temp)==null) ReHach.set(temp, element);
+                    else{
+                        flag=true;
+                        break;
+                    }
+                }
+            }
+            if(flag){
+                continue;
+            }
+            Level2Hash[index] = ReHach;
         }
         return ReHach;
     }
     @Override
     public boolean delete(String key) {
         if(!search(key)){//TODO
-            //System.out.println("cant be deleted");
+            System.out.println("cant be deleted");
             return false;
         }else {
             int index = matHash.hash(key);
@@ -104,11 +140,12 @@ public class N_linear implements PerfectHashing {
                     } else{
                       //  System.out.println("size ="+size);
                         Level2Hash[index].clear();
+                        matHashes[index]=null;
                     }
                 }
             } else {
                 int index2 = matHashes[index].hash(key);
-                if (Level2Hash[index].get(index2) == key) {
+                if ((Level2Hash[index].get(index2)).equals(key)) {
                     Level2Hash[index].set(index2, null);
                     int size = (int) (Math.sqrt(Level2Hash[index].size()) - 1);
                     //System.out.println("size2: "+ size);
@@ -118,6 +155,8 @@ public class N_linear implements PerfectHashing {
                     } else{
                     //    System.out.println("size ="+size);
                         Level2Hash[index].clear();
+                        matHashes[index]=null;
+
                     }
                 }
             }
@@ -176,7 +215,7 @@ public class N_linear implements PerfectHashing {
         test.insert("saraaaaa");
         System.out.println("hash-----finalllll----");
         test.printHash();
-        System.out.println(test.delete("sara"));
+        test.delete("sara");
 //        test.insert("saraaaaaaaaaaaa");
         System.out.println("hash-----delete----");
         test.printHash();
--- src/test/java/N_SquareTest.java
+++ src/test/java/N_SquareTest.java
@@ -1,5 +1,44 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
-class N_SquareTest {
+public class N_SquareTest {
+
+    @Test
+    public void  TestInsert(){
+        N_Square hash=new N_Square(3);
+        assertTrue(hash.insert("Maria"));
+        assertTrue( hash.insert("Mari"));
+        assertFalse( hash.insert("Mari"));
+    }
+
+    @Test
+    public void  TestSearch(){
+        N_Square hash=new N_Square(3);
+        assertTrue(hash.insert("Maria"));
+        assertTrue( hash.insert("Mari"));
+        assertTrue( hash.search("Mari"));
+        assertFalse( hash.search("Eman"));
+    }
+
+    @Test
+    public void  TestDelete(){
+        N_Square hash=new N_Square(3);
+        assertTrue(hash.insert("Maria"));
+        assertTrue( hash.insert("Mari"));
+        assertTrue( hash.delete("Mari"));
+        assertFalse( hash.delete("Eman"));
+        assertFalse( hash.search("Mari"));
+    }
+
+    @Test
+    public void  TestEmpty(){
+        N_Square hash=new N_Square(3);
+        assertFalse( hash.delete("Mari"));
+        assertFalse( hash.search("Eman"));
+    }
+
+
 
 }
