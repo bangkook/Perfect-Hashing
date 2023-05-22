c1b45ba6ee7d7ca384c64461bf6adc4679aca1e3
Maria Bassem <maria.bassem2002@gmail.com>
2023-05-22 14:34:04 +0300

    N_Linear test cases

--- src/test/java/N_linearTest.java
+++ src/test/java/N_linearTest.java
@@ -1,5 +1,43 @@
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
-class N_linearTest {
+public class N_linearTest {
+    @Test
+    public void  TestInsert(){
+        N_linear hash=new N_linear(3);
+        assertTrue(hash.insert("Eman"));
+        assertTrue(hash.insert("Sara"));
+        assertFalse(hash.insert("Sara"));
+        assertTrue(hash.insert("Nancy"));
+        assertFalse(hash.insert("Nancy"));
+    }
+
+    @Test
+    public void  TestSearch(){
+        N_linear hash=new N_linear(3);
+        assertTrue(hash.insert("Eman"));
+        assertTrue(hash.insert("Sara"));
+        assertTrue(hash.search("Eman"));
+        assertFalse(hash.search("Ema"));
+    }
+
+    @Test
+    public void  TestDelete(){
+        N_linear hash=new N_linear(3);
+        assertTrue(hash.insert("Maria"));
+        assertTrue(hash.insert("Ema"));
+        assertTrue(hash.delete("Ema"));
+        assertFalse(hash.delete("Sara"));
+        assertFalse(hash.search("Ema"));
+    }
+
+    @Test
+    public void  TestEmpty(){
+        N_linear hash=new N_linear(3);
+        assertFalse(hash.delete("Sara"));
+        assertFalse(hash.search("Nancy"));
+        assertFalse(hash.search("Eman"));
+    }
 
 }
